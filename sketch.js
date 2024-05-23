cfaf5c2d928b4a53ac116b0af8de2c6c3c0baa44
rle253 <rle253@cs.washington.edu>
2024-05-23 01:42:41 -0700

    net controller code

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+
 // sprite globals
 let sprites;
 let nextSpawnDistance;
@@ -10,17 +11,44 @@ let video;
 let predictions;
 let bound = [];
 
+let netControllerData;
+let net;
+
+let hasSetup = false;
+
+const DEFAULT_NET_SIZE = 75;
+
+net = {
+  x: Number(300 / 2),
+  y: Number(200 / 2),
+  xSpeed: 0,
+  ySpeed: 0,
+  diameter: DEFAULT_NET_SIZE,
+}
+
+netControllerData = {
+  x: 0,
+  y: 0,
+  z: 0,
+  pressed: false,
+}
+
+
+
 // Must declare image array in preload to work
 // Invariant: # of invasive and non-invasive species must be the same.
 // Each Sprite has an image index that is the same across invasive and native variants
 function preload() {
-  invasiveImages = [loadImage('img/invasive/brown_marmorated_stinkbug.jpg'), 
+  invasiveImages = [loadImage('img/invasive/brown_marmorated_stinkbug.jpg'),
                     loadImage('img/invasive/american_bullfrog.png'),
                     loadImage('img/invasive/garlic_mustard.jpg')];
-  nativeImages = [loadImage('img/native/american_pika.jpg'), 
+  nativeImages = [loadImage('img/native/american_pika.jpg'),
                     loadImage('img/native/olympic_marmot.jpg'),
                     loadImage('img/native/canada_geese.jpg')];
 }
+
+
+
 
 function setup() {
   createCanvas(640, 480);
@@ -40,10 +68,21 @@ function setup() {
       predictions = null;
     }
   });
+
+
+
+  hasSetup = true;
+
+
 }
 
 function draw() {
   // Mirror video
+  // hasSetup = true;
+  netUpdate();
+
+
+
   push();
   translate(width,0);
   scale(-1.0,1.0);
@@ -54,9 +93,10 @@ function draw() {
   }
   pop();
 
+
   // If no sprites or last sprite has surpassed nextSpawnDistance, generate another sprite
   if (sprites.length <= 0 || sprites[sprites.length-1].x >= nextSpawnDistance){
-    sprites.push(getNewSprite()); 
+    sprites.push(getNewSprite());
     // nextSpawnDistance = random(minDistanceBetweenSprites, width/3);
     // print(nextSpawnDistance);
   }
@@ -66,9 +106,9 @@ function draw() {
     // check if sprite is in hand
     // sprite will turn red if selected
     let selected = in_poly({x: sprites[i].x, y: sprites[i].y}, bound);
-    
+
     sprites[i].update();
-    
+
     // remove sprites that have gone off the screen
     if(sprites[i].x + sprites[i].width > width){
       sprites[i].offScreen = true;
@@ -106,6 +146,8 @@ function draw() {
   if(sprites[0].offScreen) {
     sprites.splice(0, 1);
   }
+
+  drawNetCursor();
 }
 
 function resetGame(){
@@ -120,4 +162,91 @@ function getNewSprite() {
   return new Sprite(typeIndex);
 }
 
-// Code and Sprite class inspiried by https://editor.p5js.org/jonfroehlich/sketches/sFOMDuDaw 
+// Code and Sprite class inspiried by https://editor.p5js.org/jonfroehlich/sketches/sFOMDuDaw
+
+
+const ws = new WebSocket('ws://localhost:8005');
+
+ws.onopen = () => {
+  console.log('Connected to the server');
+};
+
+ws.onmessage = (event) => {
+
+  // console.log(event.data);
+  let data = event.data.split(",");
+  netControllerData = {
+    x: Number(data[0]),
+    y: Number(data[1]),
+    z: Number(data[2]),
+    pressed: false
+  }
+
+};
+
+ws.onclose = () => {
+  console.log('Disconnected from the server');
+};
+
+
+
+function netUpdate() {
+
+  // if (!hasSetup) return;
+
+  let isPressed = netControllerData.pressed;
+  let isCatching = isPressed && netControllerData.z <= -6; // -9 is fully flat / parallel to ground
+
+  if (isCatching) {
+    lockNetPosition();
+    updateCapture();
+  } else if (isPressed) {
+    lockNetPosition();
+  } else {
+    updateNetPosition();
+  }
+
+
+}
+
+function drawNetCursor() {
+  push();
+  strokeWeight(4)
+  noFill();
+  circle(net.x, net.y, net.diameter);
+  pop();
+}
+
+function lockNetPosition() {
+}
+
+function updateNetPosition() {
+  net.xSpeed = -netControllerData.y;
+  net.ySpeed = -netControllerData.z;
+
+  if (!isInNetBoundsChecker()) return;
+
+  net.x += net.xSpeed;
+  net.y += net.ySpeed;
+}
+
+function isInNetBoundsChecker() {
+
+  let nextXPosition = net.x + net.xSpeed;
+  let nextYPosition = net.y + net.ySpeed;
+
+  let netRadius = net.diameter/2;
+  let isOutOfBoundsX = nextXPosition < 0 + netRadius || nextXPosition > width - netRadius;
+  let isOutOfBoundsY = nextYPosition < 0 + netRadius || nextYPosition > height - netRadius;
+  let isInBounds = !(isOutOfBoundsX || isOutOfBoundsY);
+
+  return isInBounds;
+}
+
+function updateCapture() {
+
+}
+
+
+
+
